dedab3608abf02055baf47d5d590819e04eeecdb
ProteinJ <jaehyun516@naver.com>
2025-10-26 14:50:22 +0900

    Initial Commit

---
+++ src/main/java/com/mancool/fishingboatweb/FishingBoatWebApplication.java
@@ -0,0 +1,13 @@
+package com.mancool.fishingboatweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingBoatWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingBoatWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mancool/fishingboatweb/FishingBoatWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mancool.fishingboatweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingBoatWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
